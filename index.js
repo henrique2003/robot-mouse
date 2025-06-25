2a3c4972ce70500daaef670c621a41f6be65e28b
henrique2003 <rique.cristioglu@hotmail.com>
2025-06-25 14:38:29 -0300

    feat: movimentacao demouse

---
+++ index.js
@@ -0,0 +1,18 @@
+const robot = require("robotjs");
+
+const intervaloMovimento = 3;
+const tempoVolta = 1;
+const pixeis = 20;
+
+function moveMouseSlightly() {
+  const pos = robot.getMousePos();
+
+  robot.moveMouse(pos.x + pixeis, pos.y);
+  setTimeout(() => {
+    robot.moveMouse(pos.x, pos.y);
+  }, 1000 * tempoVolta);
+}
+
+console.log("Movimentando o mouse a cada 3 segundos...");
+
+setInterval(moveMouseSlightly, 1000 * intervaloMovimento);
